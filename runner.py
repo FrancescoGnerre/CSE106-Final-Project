f3025e5b4db97defc6e727b184ff281b8b8097f9
David Chu <dchu26@ucmerced.edu>
2021-12-12 14:35:19 -0800

    Foundation for script.js and runner.py

--- runner.py
+++ runner.py
@@ -6,10 +6,26 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///class-enrollment.sqlite"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+app.secret_key = 'keep it secret, keep it safe'
+db = SQLAlchemy(app)
+
 # Login
 @app.route("/", methods = ["GET", "POST"])
 def login():
     return render_template('login.html')
+
+# Logout
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return url_for('login')[1:]
 
 if __name__ == "__main__":
     # db.create_all() # Only need this line if db not created
--- static/script.js
+++ static/script.js
@@ -1,3 +1,20 @@
-function helloWorld(){
-    alert("Hello World")
+var curr_name = "";
+var curr_password = "";
+var home_password = "";
+
+// sets name to be inputted username
+function setName(){
+	curr_name = document.getElementById("user_name").value;
+}
+// sets password to be inputted password
+function setPassword(){
+	curr_password = document.getElementById("password").value;
+}
+// sets home password to be the real password
+function setPassword(newPassword){
+    home_password = newPassword;
+}
+// prints for debugging
+function consolePrint(x){
+	console.log(x);
 }
