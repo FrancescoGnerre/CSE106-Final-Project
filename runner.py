7ca03c6f179f3ace89923ec1eb600aac5d427100
Francesco Gnerre <francesco.gnerre@gmail.com>
2021-12-12 19:47:02 -0800

    add Users DB

--- runner.py
+++ runner.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///class-enrollment.sqlite"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.sqlite"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
 login_manager = LoginManager()
@@ -15,8 +15,27 @@ login_manager.login_view = 'login'
 app.secret_key = 'keep it secret, keep it safe'
 db = SQLAlchemy(app)
 
+
+@login_manager.user_loader
+def load_user(user_id):
+    return 0;
+    # return Users.query.filter_by(user_id = user_id).first()
+
+
+# users database.
+class Users(UserMixin, db.Model):
+    __tablename__ = "Users"
+    user_name = db.Column(db.String, primary_key=True)
+    password = db.Column(db.String, nullable=False)
+    display_name = db.Column(db.String, nullable=False)
+    user_key =db.Column(db.Integer, nullable=False)
+    # is_admin is for admin view. Only our accouts will be admin
+    is_admin = db.Column(db.Integer)
+    # nullable = True is default
+
+
 # Login
-@app.route("/", methods = ["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def login():
     return render_template('login.html')
 
@@ -26,6 +45,7 @@ def login():
 def logout():
     logout_user()
     return url_for('login')[1:]
+
 
 if __name__ == "__main__":
     # db.create_all() # Only need this line if db not created
