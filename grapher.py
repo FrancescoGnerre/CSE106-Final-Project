a6bfa1189818188fff80a3d3a58200029da816a8
Francesco Gnerre <francesco.gnerre@gmail.com>
2021-12-13 23:17:02 -0800

    add grapher for pie and bar no connection to file, no formating, still debugging inputs

--- grapher.py
+++ grapher.py
@@ -31,9 +31,14 @@ def graph_bar(csv, col_x, col_y, name, legend_name, x_label, y_label):
     matPlot.show()
 
 
-def graph_line(csv, num_cols, *cols, col_x):
+def graph_line(csv, num_cols, *cols, col_x, legend_name, name, x_label, y_label):
     # csv is the file
     # num_cols is the number of columns we care about about in the y
     # *cols is an array of the columns we will use for the y
     # col_x is the column we use for the x
+    # name is the graph name
+    # legend_name is the legend name
+    # x_label is the x axis label
+    # y_label is the y axis label
+
     print("will graph a line graph of %d lines, eventually" % num_cols)
